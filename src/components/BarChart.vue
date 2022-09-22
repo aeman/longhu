da8fdb51444f02bed9f2dd6e3b6a04fcc27bd5dc
chenxuan <mr.chenxuan@gmail.com>
2022-09-22 20:53:02 +0800

    first commit

---
+++ src/components/BarChart.vue
@@ -0,0 +1,102 @@
+<template>
+    <div>
+        <el-row>
+            <el-col :span="24">
+                <el-card>
+                    <template #header><span>每日操作</span></template>
+                    <div class="el-table el-table--enable-row-hover el-table--medium">
+                        <div id="myChart" ref="chart" style="height: 420px" />
+                    </div>
+                </el-card>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script setup>
+import * as echarts from 'echarts';
+import { onMounted, ref } from 'vue';
+
+onMounted(() => getList());
+
+function getList() {
+    let chart = echarts.init(document.getElementById("myChart"), "macarons");
+    chart.setOption({
+
+        title: {
+
+            text: '今日话务统计'
+
+        },
+
+        tooltip: {
+
+            trigger: 'axis',
+
+            axisPointer: {
+
+                type: 'shadow'
+
+            }
+
+        },
+
+        grid: {
+
+            left: '3%',
+
+            right: '4%',
+
+            bottom: '3%',
+
+            containLabel: true
+
+        },
+
+        xAxis: [
+
+            {
+
+                type: 'category',
+
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+
+                axisTick: {
+
+                    alignWithLabel: true
+
+                }
+
+            }
+
+        ],
+
+        yAxis: [
+
+            {
+
+                type: 'value'
+
+            }
+
+        ],
+
+        series: [
+
+            {
+
+                name: '直接访问',
+
+                type: 'bar',
+
+                barWidth: '60%',
+
+                data: [10, 52, 200, 334, 390, 330, 220]
+
+            }
+
+        ]
+
+    });
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import App from './App.vue'
+
+import './assets/main.css'
+import axios from 'axios'
+
+const app = createApp(App)
+app.config.globalProperties.$axios = axios
+
+app.use(ElementPlus)
+app.mount('#app')
